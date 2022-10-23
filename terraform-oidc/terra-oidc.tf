982440ef41e6f6aff600e97d0eca35ba82f96327
luis.mendez <luismendeze@hotmail.com>
2022-10-22 20:27:46 -0500

    changes for today

--- terraform-oidc/terra-oidc.tf
+++ terraform-oidc/terra-oidc.tf
@@ -5,7 +5,7 @@ module "gh_oidc" {
   provider_id = "example-gh-provider"
   sa_mapping = {
     "luis-service-account" = {
-      sa_name   = "projects/my-project/serviceAccounts/luis-service-account@triggering-a-198-393873a0.iam.gserviceaccount.com"
+      sa_name   = "projects/triggering-a-198-393873a0/serviceAccounts/luis-service-account@triggering-a-198-393873a0.iam.gserviceaccount.com"
       attribute = "attribute.repository/luismendezescobar/learningActions"
     }
   }
